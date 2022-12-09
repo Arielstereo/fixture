00296579e4fc7a667e3dbc5ed2b70c08c54c7ba9
Ariel Martinez <arielstereo@msn.com>
2022-12-09 11:30:42 -0300

    Block cuartos

--- pages/pronosticos/cuartos.jsx
+++ pages/pronosticos/cuartos.jsx
@@ -366,14 +366,14 @@ export default function Finals4({ cuartos }) {
               </span>
             )}
             <button
-              disabled={isSubmit}
-              className={
-                isSubmit
-                  ? "text-gray-100 font-semibold bg-gray-600 border-2 border-white py-2 px-16 rounded-lg mx-12 mt-4"
-                  : "text-gray-800 font-semibold bg-yellow-400 hover:bg-transparent hover:text-white border-2 border-white py-2 px-16 rounded-lg mx-12 mt-4"
-              }
-              // disabled="true"
-              // className="text-gray-100 font-semibold bg-gray-600 border-2 border-white py-2 px-16 rounded-lg mx-12 mt-4"
+              // disabled={isSubmit}
+              // className={
+              //   isSubmit
+              //     ? "text-gray-100 font-semibold bg-gray-600 border-2 border-white py-2 px-16 rounded-lg mx-12 mt-4"
+              //     : "text-gray-800 font-semibold bg-yellow-400 hover:bg-transparent hover:text-white border-2 border-white py-2 px-16 rounded-lg mx-12 mt-4"
+              // }
+              disabled="true"
+              className="text-gray-100 font-semibold bg-gray-600 border-2 border-white py-2 px-16 rounded-lg mx-12 mt-4"
             >
               {isSubmit ? (
                 <div className="flex gap-6">
